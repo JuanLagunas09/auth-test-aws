e83df6fdc9c4e4859c77bfa32666dcf9254c1e51
JuanLagunas09 <lagunasjuan_isc09@hotmail.com>
2024-11-09 13:22:23 -0600

    hello Actions

--- src/servicies/AuthService.ts
+++ src/servicies/AuthService.ts
@@ -1,7 +1,7 @@
 export class AuthService {
     async hello(): Promise<string> {
         try {
-            return "Hello World service!";
+            return "Hello World With Actions!";
         } catch (error) {
             console.log(error);
             throw error;
